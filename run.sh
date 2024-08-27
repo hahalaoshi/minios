26e301c359f07056708b0e466524528df9d5f964
Zhen Wang <wangzhcs@163.com>
2024-08-27 09:38:31 +0800

    enable 9p-device to share folder with host machine

--- run.sh
+++ run.sh
@@ -7,5 +7,7 @@ sudo qemu-system-aarch64 -M virt,virtualization=true,gic-version=3 \
 	-append "console=ttyAMA0" \
 	-initrd ~/work/minios/output/initrd \
 	-gdb tcp::1287 \
+	-device virtio-net,netdev=net0 \
         -netdev type=tap,id=net0,ifname=tap0,script=no,downscript=no \
-	-device virtio-net,netdev=net0
+	-device virtio-9p-device,id=fs0,fsdev=fsdev0,mount_tag=hostshare \
+	-fsdev local,security_model=passthrough,id=fsdev0,path=/home/momo/work
